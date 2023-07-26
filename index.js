d689bd17241e4bbf02f98fc6b513f8a5904f5e03
Joell Malone <joellmalone000@gmail.com>
2023-07-26 12:59:29 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM loaded ');
+
+})
+console.log('Before the DOM loads');
+
+function changeText(text) {
+    let id = document.getElementById('text');
+    id.textContent = 'This is really cool!'
+}
+document.addEventListener('DOMContentLoaded', changeText);
